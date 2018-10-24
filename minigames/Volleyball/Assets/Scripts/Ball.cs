61cb1199d79133cfc873354c8f0882a8a53aaeb6
David Luis Alonso <david.luis.87@gmail.com>
2018-10-24 22:12:17 +0200

    volleyball

---
+++ minigames/Volleyball/Assets/Scripts/Ball.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public float ballReboundSpeed = 2;
+
+    private Rigidbody2D body;
+
+	void Start ()
+    {
+        body = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Head")
+        {
+            float speed = collision.transform.GetComponentInParent<Rigidbody2D>().velocity.x;
+            if (speed == 0)
+            {
+                speed = body.velocity.x;
+            }
+            body.velocity = new Vector2(speed, ballReboundSpeed);
+        }
+        else if (collision.tag == "Floor")
+        {
+            body.position = new Vector2(-1.8f, 0.926f);
+            body.velocity = new Vector2(0, 3);
+        }
+        else if (collision.tag == "Bounds")
+        {
+            if (collision.transform.position.x < transform.position.x && body.velocity.x < 0)
+            {
+                body.velocity = new Vector2(-body.velocity.x, body.velocity.y);
+            }
+            else if (collision.transform.position.x > transform.position.x && body.velocity.x > 0)
+            {
+                body.velocity = new Vector2(-body.velocity.x, body.velocity.y);
+            }
+        }
+    }
+}
---
+++ minigames/Volleyball/Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float jumpSpeed = 7.0f;
+    public float movementSpeed = 3;
+    public int playerIndex = 1;
+
+    private Rigidbody2D body;
+
+	void Start ()
+    {
+        body = GetComponent<Rigidbody2D>();
+	}
+
+    void Update()
+    {
+        float horizontalSpeed = Input.GetAxisRaw("Horizontal" + playerIndex.ToString());
+
+        float verticalSpeed = body.velocity.y;
+        if (Input.GetButton("Jump" + playerIndex.ToString()) && CanJump())
+        {
+            verticalSpeed = jumpSpeed;
+        }
+
+        body.velocity = new Vector2(horizontalSpeed * movementSpeed, verticalSpeed);
+    }
+
+
+    private bool CanJump()
+    {
+        return body.velocity.y == 0;
+    }
+}
