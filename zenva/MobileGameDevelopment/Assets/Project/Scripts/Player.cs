224b2b84596cda9b7c1c47de20dc604cfa2e1ff6
David Luis Alonso <david.luis.87@gmail.com>
2018-11-14 22:01:31 +0100

    Mobile game development project

---
+++ zenva/MobileGameDevelopment/Assets/Project/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float speed;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        bool isPressingLeft = Input.GetAxis("Horizontal") < 0 || (Input.GetMouseButton(0) && (Input.mousePosition.x / Screen.width) <= 0.5f);
+        bool isPressingRight = Input.GetAxis("Horizontal") > 0 || (Input.GetMouseButton(0) && (Input.mousePosition.x / Screen.width) > 0.5f);
+
+        if (isPressingLeft)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-speed, 0);
+        }
+        else if (isPressingRight)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 0);
+        }
+        else
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        }
+    }
+}
