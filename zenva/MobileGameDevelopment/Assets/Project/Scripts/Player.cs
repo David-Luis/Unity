09e81c2420ac54a79db9ba70f14d6f819d1f7fd6
David Luis Alonso <david.luis.87@gmail.com>
2018-11-19 22:05:53 +0100

    working on tests

--- zenva/MobileGameDevelopment/Assets/Project/Scripts/Player.cs
+++ zenva/MobileGameDevelopment/Assets/Project/Scripts/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -12,6 +13,12 @@ public class Player : MonoBehaviour {
 	}
 	
 	void Update ()
+    {
+        UpdateMovement();
+        CheckScreenLimits();
+    }
+
+    private void UpdateMovement()
     {
         bool isPressingLeft = Input.GetAxis("Horizontal") < 0 || (Input.GetMouseButton(0) && (Input.mousePosition.x / Screen.width) <= 0.5f);
         bool isPressingRight = Input.GetAxis("Horizontal") > 0 || (Input.GetMouseButton(0) && (Input.mousePosition.x / Screen.width) > 0.5f);
@@ -27,6 +34,21 @@ public class Player : MonoBehaviour {
         else
         {
             GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        }
+    }
+
+    private void CheckScreenLimits()
+    {
+        float spriteWidth = GetComponent<SpriteRenderer>().sprite.bounds.size.x;
+        float screenLimit = Camera.main.orthographicSize * Camera.main.aspect - spriteWidth * 0.5f;
+
+        if (transform.position.x > screenLimit)
+        {
+            transform.position = new Vector3(screenLimit, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x < -screenLimit)
+        {
+            transform.position = new Vector3(-screenLimit, transform.position.y, transform.position.z);
         }
     }
 }
---
+++ zenva/MobileGameDevelopment/Assets/Project/Scripts/Player3D.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player3D : MonoBehaviour
+{
+
+    public float speed;
+
+
+    void Update()
+    {
+        UpdateMovement();
+        CheckScreenLimits();
+    }
+
+    private void UpdateMovement()
+    {
+        Vector2 relativeMousePosition = new Vector2(Input.mousePosition.x / Screen.width, Input.mousePosition.y / Screen.height);
+        bool isPressingLeft = Input.GetAxis("Horizontal") < 0 || (Input.GetMouseButton(0) && relativeMousePosition.x <= 0.5f);
+        bool isPressingRight = Input.GetAxis("Horizontal") > 0 || (Input.GetMouseButton(0) && relativeMousePosition.x > 0.5f);
+
+        if (isPressingLeft)
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(-speed, 0);
+        }
+        else if (isPressingRight)
+        {
+            GetComponent<Rigidbody>().velocity = new Vector2(speed, 0);
+        }
+        else
+        {
+            GetComponent<Rigidbody>().velocity = Vector2.zero;
+        }
+    }
+
+    private void CheckScreenLimits()
+    {
+        float zDistance = Camera.main.transform.position.z - transform.position.z;
+
+        float leftLimit = Camera.main.ScreenToWorldPoint(new Vector3(
+            0,
+            0,
+            -zDistance / (Mathf.Cos(Camera.main.transform.localEulerAngles.x)))).x;
+
+        float rightLimit = Camera.main.ScreenToWorldPoint(new Vector3(
+            Screen.width, 
+            0, 
+            -zDistance / (Mathf.Cos(Camera.main.transform.localEulerAngles.x)))).x;
+
+        if (transform.position.x > rightLimit)
+        {
+            transform.position = new Vector3(rightLimit, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x < leftLimit)
+        {
+            transform.position = new Vector3(leftLimit, transform.position.y, transform.position.z);
+        }
+    }
+
+};
