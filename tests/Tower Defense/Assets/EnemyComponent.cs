a2637719e4b6026510730cd826d0ef8567914a58
David Luis Alonso <david.luis.87@gmail.com>
2020-10-21 19:17:30 +0200

    test

---
+++ tests/Tower Defense/Assets/EnemyComponent.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyComponent : MonoBehaviour
+{
+    private NavMeshAgent navMeshAgent;
+    public Transform targetPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshAgent.SetDestination(targetPosition.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- tests/Tower Defense/Assets/Scripts/CameraController.cs
+++ tests/Tower Defense/Assets/Scripts/CameraController.cs
@@ -5,11 +5,19 @@ public class CameraController : MonoBehaviour
     public float panSpeed = 10f;
     public float panBorderThickness = 50f;
 
-    public float scrollSpeed = 500f;
     public float minY = 5f;
     public float maxY = 20f;
 
-    // Update is called once per frame
+    public float zoomSpeed = 2000f;
+    private float zoomTargetY = 0;
+    private float zoomSmooth = 0.1f;
+    private Vector3 velocity = Vector3.zero;
+
+    private void Start()
+    {
+        zoomTargetY = transform.position.y;
+    }
+
     void Update()
     {
         if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
@@ -30,13 +38,16 @@ public class CameraController : MonoBehaviour
         }
 
         float scroll = Input.GetAxis("Mouse ScrollWheel");
+        if (Mathf.Abs(scroll) > Mathf.Epsilon)
+        {
+            Vector3 pos = transform.position;
+            zoomTargetY = pos.y - scroll * zoomSpeed * Time.deltaTime;
+            zoomTargetY = Mathf.Clamp(zoomTargetY, minY, maxY);
 
-        Vector3 pos = transform.position;
+            Debug.Log(scroll + ", " + transform.position.y + ", " + zoomTargetY);
+        }
 
-        pos.y -= scroll * scrollSpeed * Time.deltaTime;
-        pos.y = Mathf.Clamp(pos.y, minY, maxY);
-
-        transform.position = pos;
-
+        
+        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(transform.position.x, zoomTargetY, transform.position.z), ref velocity, zoomSmooth);
     }
 }
