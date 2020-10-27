72ae24182e8a6cd3f4a677aeb9b76b3cd3b4c7db
David Luis Alonso <david.luis.87@gmail.com>
2020-10-27 16:51:42 +0100

    difficulty tweaks

---
+++ tests/Tower Defense/Assets/Scripts/gameplay/EndlessWaveDefinition.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class EndlessWaveDefinition : MonoBehaviour
+{
+    public float IncreaseDurationSeconds = 1.0f;
+
+    public float DecreaseMinNextSpawnInSeconds = 0.01f;
+
+    [Range(1, 10)]
+    public float DecreaseMaxNextSpawnInSeconds = 0.01f;
+
+    public int IncreaseHealthEnemy = 10;
+}
--- tests/Tower Defense/Assets/Scripts/gameplay/EnemySpawnerComponent.cs
+++ tests/Tower Defense/Assets/Scripts/gameplay/EnemySpawnerComponent.cs
@@ -11,16 +11,22 @@ public class EnemySpawnerComponent : MonoBehaviour
     private WaveDefinition[] waves = null;
 
     [SerializeField]
+    private EndlessWaveDefinition endlessWave = null;
+
+    [SerializeField]
     private float randomDeltaStartPosition = 1;
 
     private WaveDefinition currentWave = null;
     private int currentWaveIndex;
     int increaseHelthEnemy = 0;
+    private int currentEndlessWave = 0;
 
     int nextSpawnAmount = 0;
     float waveTotalTimeSeconds = 0;
     float nextSpawnTimeSeconds = 0;
     public bool IsWaveActive { get; private set; } = false;
+
+    const float MIN_SPAWN_TIME = 0.5f;
 
     void Start()
     {
@@ -55,7 +61,10 @@ public class EnemySpawnerComponent : MonoBehaviour
     private void PrepareNextSpawn()
     {
         nextSpawnAmount = Random.Range(currentWave.MinSpawnSimultaniousEnemies, currentWave.MaxSpawnSimultaniousEnemies+1);
-        nextSpawnTimeSeconds = waveTotalTimeSeconds + Random.Range(currentWave.MinNextSpawnInSeconds, currentWave.MaxNextSpawnInSeconds);
+
+        float minSpawnTime = Mathf.Max(MIN_SPAWN_TIME, currentWave.MinNextSpawnInSeconds - currentEndlessWave * endlessWave.DecreaseMinNextSpawnInSeconds);
+        float maxSpawnTime = Mathf.Max(MIN_SPAWN_TIME, currentWave.MaxNextSpawnInSeconds - currentEndlessWave * endlessWave.DecreaseMaxNextSpawnInSeconds);
+        nextSpawnTimeSeconds = waveTotalTimeSeconds + Random.Range(minSpawnTime, maxSpawnTime);
     }
 
     private WaveDefinition GetNextWave()
@@ -63,6 +72,7 @@ public class EnemySpawnerComponent : MonoBehaviour
         currentWaveIndex++;
         if (currentWaveIndex >= waves.Length)
         {
+            currentEndlessWave++;
             currentWaveIndex = waves.Length-1;
         }
 
@@ -88,13 +98,14 @@ public class EnemySpawnerComponent : MonoBehaviour
 
             GameObject newEnemy = Instantiate(enemy, spawnPosition, Quaternion.identity, transform);
             EnemyComponent enemyComponent = newEnemy.GetComponent<EnemyComponent>();
-            enemyComponent.AddExtraHealth(increaseHelthEnemy);
+            enemyComponent.AddExtraHealth(increaseHelthEnemy + currentEndlessWave * endlessWave.IncreaseHealthEnemy);
             enemyComponent.SetDestination(playerBase.transform);
         }
     }
 
     private void FinishWave()
     {
+        waveTotalTimeSeconds = 0;
         IsWaveActive = false;
     }
 }
